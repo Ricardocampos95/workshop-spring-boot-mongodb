880cd887bb911ad89fe4f2b6eb4105ff90420c0d
Ricardocampos95 <ricardo.eteo@gmail.com>
2025-08-05 22:18:16 +0100

    Endpoint to return user posts

--- src/main/java/com/campos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/campos/workshopmongo/resources/UserResource.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.campos.workshopmongo.domain.Post;
 import com.campos.workshopmongo.domain.User;
 import com.campos.workshopmongo.dto.UserDTO;
 import com.campos.workshopmongo.services.UserService;
@@ -70,6 +71,13 @@ public class UserResource {
 		
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findUserById(id);
+
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
