f7e8b11533de8c3458a33be3849258b922051f88
Ricardocampos95 <ricardo.eteo@gmail.com>
2025-08-06 21:55:56 +0100

    Consulting with simple Query methots

--- src/main/java/com/campos/workshopmongo/resources/PostResource.java
+++ src/main/java/com/campos/workshopmongo/resources/PostResource.java
@@ -9,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.campos.workshopmongo.domain.Post;
 import com.campos.workshopmongo.domain.User;
 import com.campos.workshopmongo.repository.UserRepository;
+import com.campos.workshopmongo.resources.util.URL;
 import com.campos.workshopmongo.services.PostService;
 
 @RestController
@@ -42,8 +44,29 @@ public class PostResource {
 		for (User user : users) {
 			posts.addAll(user.getPosts());
 		}
-		
 		return ResponseEntity.ok().body(posts);
 	}
 
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle (@RequestParam(defaultValue="") String text){
+		
+		text = URL.decodeParam(text);
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
